322d733954d30e2a1204c2fc8c5241d25cb5b47a
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-12 11:21:33 +0200

    Handle file encoding. Closes #01

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use csv::ReaderBuilder;
+use encoding::all::ISO_8859_15;
+use encoding::Encoding;
 use std::fs::File;
 use std::io::Read;
 
@@ -6,8 +8,8 @@ fn main() {
     let mut file_content = Vec::new();
     let mut file = File::open("test.csv").expect("Unable to open file");
     file.read_to_end(&mut file_content).expect("Unable to read");
-    let file_as_text = String::from_utf8_lossy (&file_content);
-    println!("{:?}", file_as_text);
+    let encoded_file = ISO_8859_15.decode(&file_content, encoding::DecoderTrap::Replace).expect("Could not process file");
+    println!("{:?}", encoded_file);
     
     // let input = fs::read_to_string("test.csv").expect("No file found");
     // let mut parts = input.split("Date;Libellé;Montant(EUROS)");
