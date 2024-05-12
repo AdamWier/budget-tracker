8d8ddebc9030f7ef2c7ff7348127cb84272b94e2
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-12 11:31:26 +0200

    Print transaction records from raw file. Closes #01

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,9 @@ fn main() {
     let mut file = File::open("test.csv").expect("Unable to open file");
     file.read_to_end(&mut file_content).expect("Unable to read");
     let encoded_file = ISO_8859_15.decode(&file_content, encoding::DecoderTrap::Replace).expect("Could not process file");
-    println!("{:?}", encoded_file);
+    let parts: Vec<&str> = encoded_file.split("\r\n\r\n").collect();
     
-    // let input = fs::read_to_string("test.csv").expect("No file found");
-    // let mut parts = input.split("Date;Libellé;Montant(EUROS)");
-    // parts.for_each(|x| println!("{:?}", x));
-    // let mut part_1_file = File::create("part1.csv").expect("File cannot be created");
-    // let mut reader = ReaderBuilder::new().delimiter(b';').from_path("test.csv").expect("No file found");
-    // reader.records().for_each(|x| println!("{:?}", x));
+    let transactions = parts.get(1).expect("No transactions");
+    let mut reader = ReaderBuilder::new().delimiter(b';').from_reader(transactions.as_bytes());
+    reader.records().for_each(|x| println!("{:?}", x));
 }
