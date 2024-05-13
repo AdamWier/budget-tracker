57a7a8aaad45440fcc87bfbaea0af6dafff401a4
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-13 19:55:35 +0200

    Fix bad calculation with periods in big numbers. Closes #01

--- src/csv_parser/models/deserializers.rs
+++ src/csv_parser/models/deserializers.rs
@@ -8,7 +8,7 @@ where
     let error = format!("Error parsing amount {}", value);
 
     let parts: Vec<&str> = value.split(',').collect();
-    let main_part = parts.first().expect(&error).parse::<f32>().expect(&error);
+    let main_part = parts.first().expect(&error).replace('.', "").parse::<f32>().expect(&error);
     let decimal_part = parts.get(1).expect(&error).parse::<f32>().expect(&error) / 100.0;
     Ok(main_part + decimal_part)
 }
