1213eec543d79f7c1872231c965f7d4d95eb5e23
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-23 12:11:46 +0200

    Move layout into seperate function. Closes #05

--- src/ui/components/layout.rs
+++ src/ui/components/layout.rs
@@ -1,13 +1,15 @@
+use std::rc::Rc;
+
 use ratatui::{
-    layout::{Alignment, Constraint, Direction, Layout},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
     style::{Color, Style},
     text::Text,
     widgets::{Block, Borders, List, Paragraph},
     Frame,
 };
 
-use crate::csv::models::ParseResult;
 use super::Component;
+use crate::csv::models::ParseResult;
 
 pub struct MainLayout {
     parse_result: ParseResult,
@@ -22,16 +24,17 @@ impl MainLayout {
 }
 
 impl Component for MainLayout {
+    fn get_layout(&mut self, frame: &mut Frame) -> Rc<[Rect]>{
+        Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .split(frame.size())
+    }
     fn render(&mut self, frame: &mut Frame<'_>) {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(3),
-                Constraint::Min(1),
-                Constraint::Length(3),
-            ])
-            .split(frame.size());
-
         let block = Block::default()
             .borders(Borders::ALL)
             .style(Style::default().fg(Color::Rgb(255, 176, 0)));
@@ -58,8 +61,10 @@ impl Component for MainLayout {
         .alignment(Alignment::Center)
         .block(block.clone());
 
-        frame.render_widget(title, chunks[0]);
-        frame.render_widget(list, chunks[1]);
-        frame.render_widget(balance, chunks[2])
+        let [title_chunk, transaction_chunk, balance_chunk] = *self.get_layout(frame) else {panic!()};
+
+        frame.render_widget(title, title_chunk);
+        frame.render_widget(list, transaction_chunk);
+        frame.render_widget(balance, balance_chunk)
     }
 }
--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -1,7 +1,10 @@
 pub mod layout;
 
-use ratatui::Frame;
+use std::rc::Rc;
+
+use ratatui::{layout::Rect, Frame};
 
 pub trait Component {
+    fn get_layout(&mut self, frame: &mut Frame) -> Rc<[Rect]>;
     fn render(&mut self, f: &mut Frame);
 }
