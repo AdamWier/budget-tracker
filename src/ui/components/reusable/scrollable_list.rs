f17c323987c094ac4a6749a0712cfadb2461628c
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-25 11:20:21 +0200

    Select items on the list. Closes #06

--- src/ui/components/reusable/scrollable_list.rs
+++ src/ui/components/reusable/scrollable_list.rs
@@ -1,8 +1,10 @@
+use std::cmp;
+
 use color_eyre::eyre::Result;
 use crossterm::event::KeyCode;
 use ratatui::{
     layout::Rect,
-    style::{Color, Style},
+    style::{Color, Modifier, Style},
     widgets::{Block, Borders, List, ListState},
     Frame,
 };
@@ -20,31 +22,53 @@ pub struct ScrollableList {
 
 impl ScrollableList {
     pub fn init(list_items: Vec<String>, up_button: KeyCode, down_button: KeyCode) -> Self {
+        let mut list_state = ListState::default();
+        list_state.select(Some(0));
+
         Self {
             list_items,
             up_button,
             down_button,
-            list_state: ListState::default(),
+            list_state,
             list_screen_lines: 0,
         }
     }
-    pub fn scroll_down(&mut self) -> Result<()> {
+    fn scroll_down(&mut self) -> Result<()> {
         let transaction_list_max = self.list_items.len().saturating_sub(self.list_screen_lines);
-        let new_offset = *self.list_state.offset_mut() + 1usize;
+        let new_offset = self.list_state.offset() + 1usize;
         *self.list_state.offset_mut() = if new_offset <= transaction_list_max {
             new_offset
         } else {
             transaction_list_max
         };
+
+        self.select_next()?;
+
         Ok(())
     }
-    pub fn scroll_up(&mut self) -> Result<()> {
+    fn scroll_up(&mut self) -> Result<()> {
         let new_offset = if self.list_state.offset() == 0usize {
             0
         } else {
             self.list_state.offset() - 1usize
         };
         *self.list_state.offset_mut() = new_offset;
+
+        self.select_previous()?;
+        Ok(())
+    }
+    fn select_next(&mut self) -> Result<()> {
+        let new_selected_index = self.list_state.selected().unwrap_or(0).saturating_add(1);
+        let next_select = cmp::min(new_selected_index, self.list_items.len() - 1);
+        self.list_state.select(Some(next_select));
+
+        Ok(())
+    }
+    fn select_previous(&mut self) -> Result<()> {
+        let new_selected_index = self.list_state.selected().unwrap_or(0).saturating_sub(1);
+        let previous_select = cmp::max(new_selected_index, 0);
+        self.list_state.select(Some(previous_select));
+
         Ok(())
     }
 }
@@ -71,7 +95,8 @@ impl Component for ScrollableList {
 
         let list = List::new(self.list_items.to_owned())
             .style(Style::default().fg(Color::Rgb(255, 176, 0)))
-            .block(block);
+            .block(block)
+            .highlight_style(Style::default().add_modifier(Modifier::REVERSED));
 
         frame.render_stateful_widget(list, chunk, &mut self.list_state);
     }
