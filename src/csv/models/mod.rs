e7418e03d111297d523969fe1623d209d804b80c
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-26 19:14:48 +0200

    Move models into their own files. Closes #06

--- src/csv/models/mod.rs
+++ src/csv/models/mod.rs
@@ -1,45 +1,13 @@
+mod budget_item;
 mod deserializers;
 pub mod list_item;
+mod transaction;
 
-use ratatui::text::Text;
-use serde::Deserialize;
-
-use self::list_item::ListItem;
-
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "PascalCase")]
-pub struct Transaction {
-    pub date: String,
-    #[serde(rename = "Libellé")]
-    pub label: String,
-    #[serde(
-        rename = "Montant(EUROS)",
-        deserialize_with = "deserializers::deserialize_amount"
-    )]
-    pub amount: f32,
-}
-
-impl ListItem for Transaction {
-    fn get_list_label(&self) -> ratatui::prelude::Text {
-        Text::raw(format!("{} - {} - {}", self.date, self.label, self.amount))
-    }
-}
+pub use budget_item::BudgetItem;
+pub use transaction::Transaction;
 
 #[derive(Debug, Default, Clone)]
 pub struct ParseResult {
     pub transactions: Vec<Transaction>,
     pub balance: f32,
-}
-
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "PascalCase")]
-pub struct BudgetItem {
-    pub label: String,
-    pub amount: f32,
-}
-
-impl ListItem for BudgetItem {
-    fn get_list_label(&self) -> ratatui::prelude::Text {
-        Text::raw(format!("{} - {}", self.label, self.amount))
-    }
 }
