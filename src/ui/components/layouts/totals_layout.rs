8c59762fcd4e90266d976ae6ca59f975494b832b
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-05-29 19:48:49 +0200

    Remove unnecessary steps. Closes #11

--- src/ui/components/layouts/totals_layout.rs
+++ src/ui/components/layouts/totals_layout.rs
@@ -35,8 +35,8 @@ impl TotalsLayout {
             assigned_transactions,
         }
     }
-    fn get_code_total_pairs(&self) -> Vec<(String, f32, f32)> {
-        let budget_items_to_total = self
+    fn get_code_total_information(&self) -> Vec<(String, f32, f32)> {
+        let mut budget_items_to_total = self
             .budget_items
             .iter()
             .filter(|x| x.setting == BudgetItemType::MULTI);
@@ -58,12 +58,11 @@ impl TotalsLayout {
         let mut total_information: Vec<(String, f32, f32)> = Vec::new();
         for (key, chunk) in assigned_transactions_by_code {
             let budget_item = budget_items_to_total
-                .clone()
                 .find(|x| x.code == *key)
                 .unwrap();
             let total = chunk
                 .iter()
-                .fold(0f32, |accu, transaction| accu + transaction.amount);
+                .fold(0.0, |accu, transaction| accu + transaction.amount);
             total_information.push((
                 budget_item.label.to_string(),
                 total.mul_add(-1.0, 0.0),
@@ -88,23 +87,21 @@ impl Component for TotalsLayout {
     }
     fn render(&mut self, frame: &mut Frame<'_>, area: Rect) {
         let paragraphs = self
-            .get_code_total_pairs()
-            .iter()
+            .get_code_total_information()
+            .into_iter()
             .map(|(code, total, amount)| {
                 Paragraph::new(Text::styled(
                     format!("{}: {}/{}", code, total, amount),
                     Style::default().fg(Color::Rgb(255, 176, 0)),
                 ))
                 .alignment(Alignment::Center)
-            })
-            .collect::<Vec<Paragraph>>();
+            });
 
         self.sections = paragraphs.len() as u16;
 
         let layout = self.get_layout(area);
 
         paragraphs
-            .iter()
             .enumerate()
             .for_each(|(index, paragraph)| frame.render_widget(paragraph, layout[index]))
     }
