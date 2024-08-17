159920d293f61029402ec363461869b24760f4e4
AdamWier <38018787+AdamWier@users.noreply.github.com>
2024-08-17 20:22:27 +0200

    Rename variables, change position of red in totally overspent

--- src/ui/components/layouts/totals/total_information.rs
+++ src/ui/components/layouts/totals/total_information.rs
@@ -20,7 +20,7 @@ impl TotalInformation {
         } else if self.max_to_date >= self.total {
             self.get_underspent_data()
         } else if self.total > self.budget_amount {
-            self.get_totally_overspent()
+            self.get_totally_overspent_data()
         } else {
             self.get_overspent_data()
         };
@@ -49,71 +49,75 @@ impl TotalInformation {
         ]
     }
     fn get_underspent_data(&self) -> Vec<Data> {
-        let diff = self.max_to_date - self.total;
+        let spent = self.total;
+        let underspent_by = self.max_to_date - spent;
+        let left_for_next_week = self.projected_spending - self.max_to_date;
+        let unspent = self.budget_amount - spent - underspent_by - left_for_next_week;
         vec![
             Data {
                 label: "Spent".into(),
-                value: self.total,
+                value: spent,
                 color: Some(self.get_orange().into()),
                 fill: FILLER,
             },
             Data {
                 label: "Left to spend to date".into(),
-                value: diff,
+                value: underspent_by,
                 color: Some(Color::Green.into()),
                 fill: FILLER,
             },
             Data {
                 label: "What's left for a week from now".into(),
-                value: self.projected_spending - diff,
+                value: left_for_next_week,
                 color: Some(Color::Yellow.into()),
                 fill: FILLER,
             },
             Data {
                 label: "Unspent".into(),
-                value: (0 as f32).max(self.budget_amount - self.total - (diff)),
+                value: unspent,
                 color: Some(Color::White.into()),
                 fill: FILLER,
             },
         ]
-        
     }
     fn get_overspent_data(&self) -> Vec<Data> {
-        let diff = self.total - self.max_to_date;
+        let overspent = self.total - self.max_to_date;
+        let unspent = self.budget_amount - self.total;
         vec![
             Data {
-                label: "Spent".into(),
+                label: "Max allowed to date".into(),
                 value: self.max_to_date,
                 color: Some(Color::Yellow.into()),
                 fill: FILLER,
             },
             Data {
                 label: "Overspent".into(),
-                value: diff,
+                value: overspent,
                 color: Some(Color::Red.into()),
                 fill: FILLER,
             },
             Data {
                 label: "Unspent".into(),
-                value: self.budget_amount - self.total,
+                value: unspent,
                 color: Some(Color::White.into()),
                 fill: FILLER,
             },
         ]
     }
-    fn get_totally_overspent(&self) -> Vec<Data> {
-        let diff = self.total - self.budget_amount;
+    fn get_totally_overspent_data(&self) -> Vec<Data> {
+        let over_max = self.total - self.budget_amount;
+        let rest_of_chart = self.budget_amount - over_max;
         vec![
             Data {
-                label: "Spent".into(),
-                value: self.budget_amount - diff,
-                color: Some(Color::Yellow.into()),
+                label: "Over max".into(),
+                value: over_max,
+                color: Some(Color::Red.into()),
                 fill: FILLER,
             },
             Data {
-                label: "Over max".into(),
-                value: diff,
-                color: Some(Color::Red.into()),
+                label: "Spent".into(),
+                value: rest_of_chart,
+                color: Some(Color::Yellow.into()),
                 fill: FILLER,
             },
         ]
